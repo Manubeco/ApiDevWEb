d39f064fe9a1e609092b3653573a4640e9e49e36
ManuBeco <emanuelbeco@outlook.com>
2022-11-29 22:00:57 -0300

    finalização dos metodos

--- functions.py
+++ functions.py
@@ -2,40 +2,8 @@ from flask import Flask, jsonify, make_response, request
 from bDados import Conta
 
 
-# def add_user():
-#     new_user = request.json
-#
-#     Conta.append(newUser)
-#
-#     return make_response(
-#         jsonify(usuario=nome,
-#                 mensagem="Conta cadastrada com sucesso!"
-#                 )
-#     )
-
-
-def alterar_conta(account_id):
-    nova_conta = {
-        "id": 3,
-        "Nome": "Fernando",
-        "Email": "anagabip@gmail.com",
-        "Senha": "quadrado3245"
-    }
-
+def apagar():
     for i in range(len(Conta)):
-        for k, v in Conta[i].items():
-            if v == account_id:
-                Conta[i] = nova_conta
-
-
-def procura_conta(id_conta):
-    for i in range(len(Conta)):
-        for k, v in Conta[i].items():
-            if v == id_conta:
-                print(Conta[id_conta])
-
-# @app.route('/exibir_conta', methods=["GET"])
-# def exibir_conta():
-#     return make_response(
-#         jsonify(Conta)
-#     )
+        for v in Conta[i].values():
+            if v == 1:
+                Conta.pop[i]
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from flask import Flask, jsonify, make_response, request
 from bDados import Conta
-from functions import alterar_conta,procura_conta
 
 app = Flask(__name__)
+
+
+
+@app.route('/conta', methods=["GET"])
+def exibir_conta():
+
+    return make_response(jsonify(Conta))
 
 
 @app.route('/conta', methods=["POST"])
@@ -11,28 +17,27 @@ def add_user():
     Conta.append(new_user)
 
     return make_response(
-        jsonify(mensagem="Conta cadastrada com sucesso!")
-    )
+        jsonify(mensagem="Conta cadastrada com sucesso!"))
 
-
-@app.route('/conta', methods=["GET"])
-def exibir_conta():
-
-    return make_response(jsonify(Conta))
 
 @app.route('/conta', methods=["PUT"])
 def alterar_conta():
     nova_conta = request.get_json()
-
+    
     for i in range(len(Conta)):
-        for k, v in Conta[i].items():
-            if v == 1:
+        for v in Conta[i].values():
+            if v == 2:
                 Conta[i] = nova_conta
+                return "Conta alterada com sucesso!"
 
-    return "conta alterada"
+    
+@app.route('/conta', methods=["DELETE"])
+def apagar_conta():
+     for i in range(len(Conta)):
+        for v in Conta[i].values():
+            if v == 1:
+                Conta.pop(i)
+                return make_response(jsonify(mensagem = "Conta apagada com sucesso!"))     
 
-
-
-
-
+   
 app.run()

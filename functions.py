fb9234c18e018c72b2d17e2e8d13c115a346f38d
ManuBeco <emanuelbeco@outlook.com>
2022-11-29 22:11:24 -0300

    corrigindo arquivos

--- functions.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify, make_response, request
-from bDados import Conta
-
-
-def apagar():
-    for i in range(len(Conta)):
-        for v in Conta[i].values():
-            if v == 1:
-                Conta.pop[i]
